9b829444be4a1d274040f07e58d2479b86cb88a1
Elena-1717 <kea5599@mail.ru>
2024-07-05 16:21:52 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,8 +1,10 @@
-my_string = input("Введите название: ")
-print('Название строки:', my_string)
+#my_string = input("Введите название: ")
+#print('Название строки:', my_string)
+#my_string = 'Введите название'
+#print('введите название'.upper())
+#print('введите значение'.lower())
+#print('введите значение'.replace(' ', ''))
 my_string = 'Введите название'
-print('введите название'.upper())
-print('введите значение'.lower())
-print('введите значение'.replace(' ', ''))
-
+print(my_string[0])
+print(my_string[-1])
 
